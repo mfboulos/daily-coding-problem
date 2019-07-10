dc46e0b312cae7c5e4000f213d8ff2130ccf3b59
mfboulos <mboulos95@gmail.com>
2019-07-10 16:16:32 -0400

    Wrote intended solution using left and right products

--- challenges/Uber/problem2/solution.py
+++ challenges/Uber/problem2/solution.py
@@ -31,5 +31,30 @@ def product_others(nums, idx):
     
     return product
 
+def intended_solution(nums):
+    """Builds growing products from both ends of nums, then multiplies them
+
+    Visualization:
+        [   a   b   c   d   ]
+    ->  [   bcd acd abd abc ]
+    =>  [   1   a   ab  abc ] = arr1
+        [   bcd cd  d   1   ] = arr2
+    
+    We then simply multiply arr1[i] * arr2[i] for each i
+    """
+
+    size = len(nums)
+    left_product = [1 for _ in range(size)]
+    right_product = [1 for _ in range(size)]
+
+    for i, num in enumerate(nums[:-1]):
+        left_product[i+1] = left_product[i] * num
+    
+    for i, num in reversed(list(enumerate(nums))[1:]):
+        right_product[i-1] = right_product[i] * num
+    
+    return [right_product[i] * left_product[i] for i in range(size)]
+
 assert process_nums([1,2,3,4,5]) == [120, 60, 40, 30, 24]
 assert process_nums_no_division([1,2,3,4,5]) == [120, 60, 40, 30, 24]
+assert intended_solution([1,2,3,4,5]) == [120, 60, 40, 30, 24]
